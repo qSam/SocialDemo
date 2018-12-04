9a26cf5193abc3f3c3f98b575c39790a5c3e6d1b
Qasim Laghari <go.jets.laghari@gmail.com>
2018-12-04 00:01:21 -0600

    Capture elements from register form

--- register.php
+++ register.php
@@ -1,3 +1,60 @@
+<?php
+  $con = mysqli_connect("localhost","root","","social");
+
+  if(mysqli_connect_errno()) {
+    echo "Failed to connect: ".mysqli_connect_errno();
+  }
+
+  //Declare variables to prevent errors
+  $fname = "";
+  $lname = "";
+  $em = "";
+  $em2 = "";
+  $password = "";
+  $password2 = "";
+  $date = "";
+  $error_array = "";
+
+  if(isset($_POST['register_button'])) {
+    // Regsiter form VALUES
+
+    //First Name
+    //Remove html tags
+    $fname = strip_tags($_POST['reg_fname']);
+    //Remove spaces
+    $fname = str_replace(' ', '', $fname);
+    //Uppercase first char
+    $fname = ucfirst(strtolower($fname));
+
+
+    //Last Name
+    $lname = strip_tags($_POST['reg_lname']);
+    $lname = str_replace(' ', '', $lname);
+    $lname = ucfirst(strtolower($lname));
+
+
+    //Email
+    $em = strip_tags($_POST['reg_email']);
+    $em = str_replace(' ', '', $em);
+    $em = ucfirst(strtolower($em));
+
+
+    //Confirmation Email
+    $em2 = strip_tags($_POST['reg_email2']);
+    $em2 = str_replace(' ', '', $em2);
+    $em2 = ucfirst(strtolower($em2));
+
+
+    //Password
+    $password = strip_tags($_POST['reg_password']);
+    $password2 = strip_tags($_POST['reg_password2']);
+
+    $date = date("Y-m-d");
+
+
+  }
+?>
+
 
 <html>
 <head>
