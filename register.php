0b081fea62c6b892c21ee979f419d9bf1d65ed08
Qasim Laghari <go.jets.laghari@gmail.com>
2018-12-05 13:16:27 -0600

    Create and check username

--- register.php
+++ register.php
@@ -105,6 +105,26 @@
       array_push($error_array, "Your password must be between 5 and 30 characters<br />");
     }
 
+    if(empty($error_array)) {
+      //Encrypt password
+      $password = md5($password);
+      //Generate username by concact firstname and lastname
+      $username = strtolower($fname."_"."$lname");
+      $check_username_query = mysqli_query($con,"SELECT username FROM users WHERE username='$username'");
+
+      $i = 0;
+      //if username exists add number to username
+      while(mysqli_num_rows($check_username_query) != 0) {
+        $i++;
+        $username = $username."_".$i;
+        $check_username_query = mysqli_query($con,"SELECT username FROM users WHERE username='$username'");
+
+      }
+
+
+
+    }
+
 
 
 
