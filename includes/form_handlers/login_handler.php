922cc5fa2a225f7cffd31fde6eb7dd1dad62be2f
Qasim Laghari <go.jets.laghari@gmail.com>
2018-12-06 14:17:51 -0600

    Handle login errors

--- includes/form_handlers/login_handler.php
+++ includes/form_handlers/login_handler.php
@@ -10,11 +10,13 @@
     $check_login_query = mysqli_num_rows($check_database_query);
 
     if($check_login_query == 1) {
-      $row - mysqli_fetch_array($check_database_query);
+      $row = mysqli_fetch_array($check_database_query);
       $username = $row['username'];
       $_SESSION['username'] = $username;
       header("Location: index.php");
       exit;
+    } else {
+      array_push($error_array, "Email or password was incorrect");
     }
 
 
--- register.php
+++ register.php
@@ -1,6 +1,7 @@
 <?php
   require 'config/config.php';
   require 'includes/form_handlers/register_handler.php';
+  require 'includes/form_handlers/login_handler.php';
 ?>
 
 
@@ -11,12 +12,24 @@
 
 <body>
   <form action="register.php" method="POST">
-    <input type="email" name="log_email" placeholder="Email address">
+    <input type="email" name="log_email" placeholder="Email address"
+    value="<?php
+    if(isset($_SESSION['log_email'])){
+      echo $_SESSION['log_email'];
+    }
+    ?>"
+    required>
     <br />
     <input type="password" name="log_password" placeholder="Password">
     <br />
     <input type="submit" name="login_button" value="Login">
+    <br />
+    <?php
+    if(in_array("Email or password was incorrect", $error_array)) {
+      echo "Email or password was incorrect";
+    }
 
+     ?>
 
   </form>
 
