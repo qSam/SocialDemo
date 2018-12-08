2503da3b3fc3b565ad56638cc40b5ae93e6a88f8
Qasim Laghari <go.jets.laghari@gmail.com>
2018-12-08 15:31:02 -0600

    Add boostrap to project

--- includes/header.php
+++ includes/header.php
@@ -12,6 +12,9 @@
 <html>
 <head>
   <title>Social Demo</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="assets/js/bootstrap.js"></script>
+  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
 </head>
 
 <body>
