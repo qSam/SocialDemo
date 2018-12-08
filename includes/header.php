73379e8114bef27cc496d788f42312a990c64f31
Qasim Laghari <go.jets.laghari@gmail.com>
2018-12-08 15:51:34 -0600

    Setup head panel

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
   require 'config/config.php';
 
   if (isset($_SESSION['username'])){
-    $userLoggedIn - $_SESSION['username'];
+    $userLoggedIn = $_SESSION['username'];
   } else {
     header("Location: register:php");
   }
@@ -12,9 +12,18 @@
 <html>
 <head>
   <title>Social Demo</title>
+  <!-- Javascript  -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="assets/js/bootstrap.js"></script>
+  <!-- CSS -->
   <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 
 <body>
+
+  <div class="top_bar">
+      <div class="logo">
+          <a href="index.php">Seattle Feed</a>
+      </div>
+  </div>
