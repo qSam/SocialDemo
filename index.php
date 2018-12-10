0eb3e3e89658af4a9ad7d128a19e4571c54f75ef
Qasim Laghari <go.jets.laghari@gmail.com>
2018-12-10 16:46:54 -0600

    Add textarea for posts

--- index.php
+++ index.php
@@ -17,6 +17,14 @@
   </div>
   </div>
 
+  <div class="main_column column">
+    <form class="post_form" action="index.php" method="POST">
+      <textarea name="post_text" id="post_text" placeholder="Got something to say"></textarea>
+      <input type="submit" name="post" id="post_button" value="Post">
+      <hr />
+    </form>
+  </div>
+
 </div>
 </body>
 </html>
