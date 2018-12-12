15c08bc93077c2f7f1b828ee6f0da7d6e10ec1a9
Qasim Laghari <go.jets.laghari@gmail.com>
2018-12-12 15:41:34 -0600

    Add logic to determine timeframe of posts

--- includes/classes/Posts.php
+++ includes/classes/Posts.php
@@ -59,13 +59,44 @@ class Post {
           $user_to = "<a href='".$row['user_to']."'>".$user_to_name."</a>";
         }
 
-        //Cheeck if user account who posted has account closed
+        //Check if user account who posted has account closed
         $added_by_obj = new User($con, $added_by);
         if($added_by_obj->isClosed()) {
           continue;
         }
         $user_details_query = mysqli_query_con($this->con, "SELECT first_name, last_name, profile_pic FROM users WHERE username='$added_by'");
         $user_row = mysqli_fetch_array($user_details_query);
+
+        //Timeframe
+        $date_time_now = date("Y-m-d H:i:s");
+        //Time of post
+        $start_date = new DateTime($date_time);
+        //Current time
+        $end_date = new DateTime($date_time_now);
+        //Get time interval
+        $interval = $start_date->diff($end_date);
+
+        if($interval->y >= 1) {
+          if($interval == 1) {
+            $time_message = $interval->y." year ago";
+          } else {
+
+              $time_message = $interval->y." years ago";
+          }
+        } else if($interval->m >= 1) {
+          if($interval->d == 0) {
+            $days = " ago";
+          } else if($interval->d == 1) {
+            $days = $interval->d." day ago";
+          } else {
+            $days = $interval->d." days ago";
+          }
+          if($interval->m == 1){
+            $time_message = $interval->m." month".$days;
+          } else {
+            $time_message = $interval->m." months".$days;
+          }
+        }
       }
     }
 
