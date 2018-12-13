7b7ab8d2af8bd6963d4ea0f229e95bfb41f00425
Qasim Laghari <go.jets.laghari@gmail.com>
2018-12-13 16:54:02 -0600

    Refactor load posts

--- index.php
+++ index.php
@@ -50,6 +50,30 @@
             $('.posts_area').html(data);
         }
       });
+
+      $(window).scroll(function(){
+        var height = $(".posts_area").height();
+        var scroll_top = $(this).scrollTop();
+        var page = $(".posts_area").find('.nextPage').val();
+        var noMorePosts = $(".posts_area").find('.noMorePosts').val();
+
+        if((document.body.scrollHeight== document.body.scrollTop + window.innerHeight) && noMorePosts == 'false'){
+          //Ajax request for loading first posts
+          var ajaxRequest = $.ajax({
+            url: "includes/handlers/ajax_load_posts.php",
+            type: "POST",
+            data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
+            cache: false,
+            success: function(response) {
+                $('.posts_area').find('.nextPage').remove();
+                $('.posts_area').find('.noMorePosts').remove();
+                $("#loading").hide();
+                $('.posts_area').append(response);
+            }
+          });
+
+        }
+      });
     });
   </script>
 
