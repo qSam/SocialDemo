9d875e24d0149fd796b9d56448524fc2c1044de2
Qasim Laghari <go.jets.laghari@gmail.com>
2018-12-13 17:35:18 -0600

    Refactor Load Posts

--- includes/classes/Posts.php
+++ includes/classes/Posts.php
@@ -163,6 +163,13 @@ class Post {
               <hr />
         ";
       }
+      if($count > $limit) {
+        $str.="<input type='hidden' class='nextPage' value='".($page+1)."'>'
+              <input type='hidden' class='noMorePosts' value='false'>";
+      } else {
+          $str.="<input type='hidden' class='noMorePosts' value='true'><p style='text-align:center;'>No more
+          posts to show!</p>";
+      }
     }
       echo $str;
     }
