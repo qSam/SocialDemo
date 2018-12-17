fac53095f976ce778735224c13f178b184aed277
Qasim Laghari <go.jets.laghari@gmail.com>
2018-12-17 16:31:46 -0600

    Update like entries in DB

--- like.php
+++ like.php
@@ -29,24 +29,33 @@
 
       $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$user_liked'");
       $row = mysqli_fetch_array($user_details_query);
+      $total_user_likes = $row['num_likes'];
 
       //Like Button
-      if(isset($_POST[like_button])){
+      if(isset($_POST['like_button'])){
         $total_likes++;
-        $query = mysqli_query($con, "UPDATE posts SET likes='$total_likes' WHERE post_id='$post_id'");
+        $query = mysqli_query($con, "UPDATE posts SET likes='$total_likes' WHERE id='$post_id'");
         $total_user_likes++;
         $user_likes = mysqli_query($con, "UPDATE users SET num_likes='$total_user_likes' WHERE username='$user_liked'");
         $insert_user = mysqli_query($con, "INSERT INTO likes VALUES('','$userLoggedIn','$post_id')");
       }
 
       //Unlike Button
+      if(isset($_POST['unlike_button'])){
+        $total_likes--;
+        $query = mysqli_query($con, "UPDATE posts SET likes='$total_likes' WHERE id='$post_id'");
+        $total_user_likes--;
+        $user_likes = mysqli_query($con, "UPDATE users SET num_likes='$total_user_likes' WHERE username='$user_liked'");
+        $insert_user = mysqli_query($con, "DELETE FROM likes WHERE username='$userLoggedIn' AND post_id='$post_id'");
+      }
+
 
       //Check for previous likes
       $check_query = mysqli_query($con, "SELECT * FROM likes where username='$userLoggedIn' AND post_id='$post_id'");
       $num_rows = mysqli_num_rows($check_query);
 
       if($num_rows > 0) {
-        echo '<form action="like.php?post_id'.$post_id.'" method="POST">
+        echo '<form action="like.php?post_id='.$post_id.'" method="POST">
                 <input type="submit" class="comment_like" name="unlike_button" value="Unlike">
                 <div class="like_value">
                   '.$total_likes.' Likes
@@ -56,7 +65,7 @@
 
         ';
       } else {
-        echo '<form action="like.php?post_id'.$post_id.'" method="POST">
+        echo '<form action="like.php?post_id='.$post_id.'" method="POST">
                 <input type="submit" class="comment_like" name="like_button" value="like">
                 <div class="like_value">
                   '.$total_likes.' Likes
