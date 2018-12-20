44b44667989a65466ca99d29ad5d7062a0d4d2f0
Qasim Laghari <go.jets.laghari@gmail.com>
2018-12-20 16:39:38 -0600

    Fix bug with isset function call

--- profile.php
+++ profile.php
@@ -1,7 +1,7 @@
 <?php
   include("includes/header.php");
 
-  if(issset($_GET['profile_username'])) {
+  if(isset($_GET['profile_username'])) {
     $username = $_GET['profile_username'];
     $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$username'");
     $user_array = mysqli_fetch_array($user_details_query);
