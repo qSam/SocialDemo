dbde43d42ae839a6f677f350ccb9acd0d06130ba
Qasim Laghari <go.jets.laghari@gmail.com>
2018-12-21 22:15:07 -0600

    Add additional css styling to profile page

--- profile.php
+++ profile.php
@@ -12,6 +12,13 @@
 
 ?>
 
+    <style type="text/css">
+      .wrapper {
+        margin-left: 0px;
+        padding-left: 0px;
+      }
+    </style>
+
     <div class="profile_left">
       <img src="<?php echo $user_array['profile_pic']; ?>">
       <div  class="profile_info">
